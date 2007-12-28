7a914cd66aff1152372d96a8a215a8a4b34df9f0
Christian Boltz <postfixadmin@cboltz.de>
2007-12-28 21:45:33 +0000

    templates/users_vacation.php: - whitespace changes to match vim: comment (ts=4)
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/trunk@276 a1433add-5e2c-0410-b055-b7f2511e0802

--- templates/users_vacation.php
+++ templates/users_vacation.php
@@ -2,33 +2,33 @@
 <div id="edit_form">
 <form name="vacation" method="post">
 <table>
-   <tr>
-      <td colspan="3"><h3><?php print $PALANG['pUsersVacation_welcome']; ?></h3></td>
-   </tr>
-   <tr>
-      <td><?php print $PALANG['pUsersVacation_subject'] . ":"; ?></td>
-      <td><input type="text" name="fSubject" value="<?php print htmlentities($tSubject, ENT_QUOTES, 'UTF-8'); ?>" /></td>
-      <td>&nbsp;</td>
-   </tr>
-   <tr>
-      <td><?php print $PALANG['pUsersVacation_body'] . ":"; ?></td>
-      <td>
+    <tr>
+        <td colspan="3"><h3><?php print $PALANG['pUsersVacation_welcome']; ?></h3></td>
+    </tr>
+    <tr>
+        <td><?php print $PALANG['pUsersVacation_subject'] . ":"; ?></td>
+        <td><input type="text" name="fSubject" value="<?php print htmlentities($tSubject, ENT_QUOTES, 'UTF-8'); ?>" /></td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td><?php print $PALANG['pUsersVacation_body'] . ":"; ?></td>
+        <td>
 <textarea rows="10" cols="80" name="fBody">
 <?php print htmlentities($tBody, ENT_QUOTES, 'UTF-8'); ?>
 </textarea>
-      </td>
-      <td>&nbsp;</td>
-   </tr>
-   <tr>
-      <td colspan="3" class="hlp_center">
-          <input class="button" type="submit" name="fAway" value="<?php print $PALANG['pUsersVacation_button_away']; ?>" />
-          <input class="button" type="submit" name="fBack" value="<?php print $PALANG['pUsersVacation_button_back']; ?>" />
-          <input class="button" type="submit" name="fCancel" value="<?php print $PALANG['exit']; ?>" />
-      </td>
-   </tr>
-   <tr>
-      <td colspan="3" class="standout"><?php print $tMessage; ?></td>
-   </tr>
+        </td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td colspan="3" class="hlp_center">
+            <input class="button" type="submit" name="fAway" value="<?php print $PALANG['pUsersVacation_button_away']; ?>" />
+            <input class="button" type="submit" name="fBack" value="<?php print $PALANG['pUsersVacation_button_back']; ?>" />
+            <input class="button" type="submit" name="fCancel" value="<?php print $PALANG['exit']; ?>" />
+        </td>
+    </tr>
+    <tr>
+        <td colspan="3" class="standout"><?php print $tMessage; ?></td>
+    </tr>
 </table>
 </form>
 </div>
