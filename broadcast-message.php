4d54548b72b3563366b22885857a704037834922
David Goodwin <david@palepurple.co.uk>
2009-04-22 14:53:35 +0000

    reindent
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/trunk@642 a1433add-5e2c-0410-b055-b7f2511e0802

--- broadcast-message.php
+++ broadcast-message.php
@@ -31,67 +31,67 @@ require_once('common.php');
 authentication_require_role('global-admin');
 
 if ($CONF['sendmail'] != 'YES') {
-   header("Location: " . $CONF['postfix_admin_url'] . "/main.php");
-   exit;
+    header("Location: " . $CONF['postfix_admin_url'] . "/main.php");
+    exit;
 }
 
 $SESSID_USERNAME = authentication_get_username();
 
 if ($_SERVER['REQUEST_METHOD'] == "POST")
 {
-   if (empty($_POST['subject']) || empty($_POST['message']) || empty($_POST['name']))
-   {
-      $error = 1;
-   }
-   else
-   {
-      $q = 'select username from mailbox union '.
-         'select goto from alias '.
-         'where goto not in (select username from mailbox)';
+    if (empty($_POST['subject']) || empty($_POST['message']) || empty($_POST['name']))
+    {
+        $error = 1;
+    }
+    else
+    {
+        $q = 'select username from mailbox union '.
+            'select goto from alias '.
+            'where goto not in (select username from mailbox)';
 
-      $result = db_query ($q);
-      if ($result['rows'] > 0)
-      {
-         mb_internal_encoding("UTF-8");
-         $b_name = mb_encode_mimeheader( $_POST['name'], 'UTF-8', 'Q');
-         $b_subject = mb_encode_mimeheader( $_POST['subject'], 'UTF-8', 'Q');
-         $b_message = base64_encode($_POST['message']);
+        $result = db_query ($q);
+        if ($result['rows'] > 0)
+        {
+            mb_internal_encoding("UTF-8");
+            $b_name = mb_encode_mimeheader( $_POST['name'], 'UTF-8', 'Q');
+            $b_subject = mb_encode_mimeheader( $_POST['subject'], 'UTF-8', 'Q');
+            $b_message = base64_encode($_POST['message']);
 
-         $i = 0;
-         while ($row = db_array ($result['result'])) {
-            $fTo = $row[0];
-            $fHeaders  = 'To: ' . $fTo . "\n";
-            $fHeaders .= 'From: ' . $b_name . ' <' . $CONF['admin_email'] . ">\n";
-            $fHeaders .= 'Subject: ' . $b_subject . "\n";
-            $fHeaders .= 'MIME-Version: 1.0' . "\n";
-            $fHeaders .= 'Content-Type: text/plain; charset=UTF-8' . "\n";
-            $fHeaders .= 'Content-Transfer-Encoding: base64' . "\n";
+            $i = 0;
+            while ($row = db_array ($result['result'])) {
+                $fTo = $row[0];
+                $fHeaders  = 'To: ' . $fTo . "\n";
+                $fHeaders .= 'From: ' . $b_name . ' <' . $CONF['admin_email'] . ">\n";
+                $fHeaders .= 'Subject: ' . $b_subject . "\n";
+                $fHeaders .= 'MIME-Version: 1.0' . "\n";
+                $fHeaders .= 'Content-Type: text/plain; charset=UTF-8' . "\n";
+                $fHeaders .= 'Content-Transfer-Encoding: base64' . "\n";
 
-            $fHeaders .= $b_message;
+                $fHeaders .= $b_message;
 
-            if (!smtp_mail ($fTo, $CONF['admin_email'], $fHeaders))
-            {
-               $tMessage .= "<br />" . $PALANG['pSendmail_result_error'] . "<br />";
+                if (!smtp_mail ($fTo, $CONF['admin_email'], $fHeaders))
+                {
+                    $tMessage .= "<br />" . $PALANG['pSendmail_result_error'] . "<br />";
+                }
+                else
+                {
+                    $tMessage .= "<br />" . $PALANG['pSendmail_result_success'] . "<br />";
+                }
             }
-            else
-            {
-               $tMessage .= "<br />" . $PALANG['pSendmail_result_success'] . "<br />";
-            }
-         }
-      }
-      include ("templates/header.php");
-      include ("templates/menu.php");
-      echo '<p>'.$PALANG['pBroadcast_success'].'</p>';
-      include ("templates/footer.php");
-   }
+        }
+        include ("templates/header.php");
+        include ("templates/menu.php");
+        echo '<p>'.$PALANG['pBroadcast_success'].'</p>';
+        include ("templates/footer.php");
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "GET" || $error == 1)
 {
-   include ("templates/header.php");
-   include ("templates/menu.php");
-   include ("templates/broadcast-message.php");
-   include ("templates/footer.php");
+    include ("templates/header.php");
+    include ("templates/menu.php");
+    include ("templates/broadcast-message.php");
+    include ("templates/footer.php");
 }
 
 /* vim: set expandtab softtabstop=3 tabstop=3 shiftwidth=3: */
