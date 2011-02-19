c2071fc7a0502ad40f39bc623a80714968de79ba
Christian Boltz <postfixadmin@cboltz.de>
2011-02-19 20:10:22 +0000

    Merging in various fixes from trunk.
    upgrade.php:
    - change domain.quota, domain.maxquota and mailbox.quota to bigint in PostgreSQL
    
    setup.php:
    - strict boolean check for $CONF['configured'] - similar to the check in login.php
    
    
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/branches/postfixadmin-2.3@969 a1433add-5e2c-0410-b055-b7f2511e0802

--- upgrade.php
+++ upgrade.php
@@ -108,7 +108,7 @@ _do_upgrade($version);
 function _do_upgrade($current_version) {
     global $CONF;
     # $target_version = preg_replace('/[^0-9]/', '', '$Revision$');
-    $target_version = 739; # hardcoded target version for 2.3 branch - increase (by one) if database changes in the branch are necessary
+    $target_version = 740; # hardcoded target version for 2.3 branch - increase (by one) if database changes in the branch are necessary
 
     if ($current_version >= $target_version) {
         # already up to date
@@ -1182,3 +1182,13 @@ function upgrade_730_pgsql() {
             FOR EACH ROW EXECUTE PROCEDURE merge_quota2();
     ");
 }
+
+function upgrade_740_pgsql() { # upgrade_968_pgsql() in SVN trunk
+    # pgsql counterpart for upgrade_169_mysql() - allow really big quota
+    $table_domain = table_by_key ('domain');
+    $table_mailbox = table_by_key('mailbox');
+    db_query_parsed("ALTER TABLE $table_domain  ALTER COLUMN quota    type bigint");
+    db_query_parsed("ALTER TABLE $table_domain  ALTER COLUMN maxquota type bigint");
+    db_query_parsed("ALTER TABLE $table_mailbox ALTER COLUMN quota    type bigint");
+}
+
