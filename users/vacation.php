6e2e132bace79e9c38e5f71aafdf390cdf009885
Christian Boltz <postfixadmin@cboltz.de>
2013-11-10 15:57:32 +0000

    remove "postfixadmin.com" in comments in lots of files
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/trunk@1558 a1433add-5e2c-0410-b055-b7f2511e0802

--- users/vacation.php
+++ users/vacation.php
@@ -6,8 +6,7 @@
  * This source file is subject to the GPL license that is bundled with
  * this package in the file LICENSE.TXT.
  *
- * Further details on the project are available at :
- *     http://www.postfixadmin.com or http://postfixadmin.sf.net
+ * Further details on the project are available at http://postfixadmin.sf.net 
  *
  * @version $Id$
  * @license GNU GPL v2 or later.
