28703935b31db0f5497794d557179e64204dd563
Christian Boltz <postfixadmin@cboltz.de>
2017-02-08 20:45:26 +0000

    3.0.2 release
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/trunk@1894 a1433add-5e2c-0410-b055-b7f2511e0802

--- functions.inc.php
+++ functions.inc.php
@@ -15,7 +15,7 @@
  * Contains re-usable code.
  */
 
-$version = '3.0';
+$version = '3.0.2';
 $min_db_version = 1835;  # update (at least) before a release with the latest function numbrer in upgrade.php
 
 /**
