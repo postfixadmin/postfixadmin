7ddac2ac4b399486888d241a13af68b92cdf77dc
David Goodwin <david@palepurple.co.uk>
2017-02-08 20:24:31 +0000

    evil hack to make vacation work while we are using Mail::Sender - see also https://sourceforge.net/p/postfixadmin/patches/136/
    git-svn-id: https://svn.code.sf.net/p/postfixadmin/code/trunk@1893 a1433add-5e2c-0410-b055-b7f2511e0802

--- VIRTUAL_VACATION/vacation.pl
+++ VIRTUAL_VACATION/vacation.pl
@@ -1,6 +1,11 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -X
+# Note  - 2017/02/08 DG :
+# Yes - I know -X (^) is not ideal.
+#       Patches are welcome to remove the dependency on Mail::Sender. 
+#       Until then, we need -X to stop it failing with warnings like 
+#       defined(@array) is deprecated at .../perl5/Mail/Sender.pm line 318. 
 #
-# Virtual Vacation 4.0r1
+# Virtual Vacation 4.0
 #
 # $Revision$
 # Originally by Mischa Peters <mischa at high5 dot net>
