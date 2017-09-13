ae56c2b70044ce1438a3c8d8e94e7f34c0f5ffef
Christian Boltz <postfixadmin@cboltz.de>
2017-09-13 18:02:56 +0200

    Fix syntax error in viewlog.php
    Reported in issue #74

--- viewlog.php
+++ viewlog.php
@@ -62,7 +62,7 @@ if ($error != 1)
    $table_log = table_by_key('log');
    $page_size = isset($CONF['page_size']) ? intval($CONF['page_size']) : 35;
 
-   $query = "SELECT timestamp,username,domain,action,data FROM $table_log WHERE domain='$fDomain' ORDER BY timestamp DESC LIMIT $page_size");
+   $query = "SELECT timestamp,username,domain,action,data FROM $table_log WHERE domain='$fDomain' ORDER BY timestamp DESC LIMIT $page_size";
    if (db_pgsql()) {
       $query = "SELECT extract(epoch from timestamp) as timestamp,username,domain,action,data FROM $table_log WHERE domain='$fDomain' ORDER BY timestamp DESC LIMIT $page_size";
    }
