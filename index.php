7f19cae57e261a2b754597bef658616be57dc12a
Christian Boltz <postfixadmin@cboltz.de>
2018-04-26 21:33:11 +0200

    Add a index.php showing a note about the "public" directory
    This avoids that users get a 403 error (if DirectoryIndex is disabled)
    or a directory listing after updating to 3.2.
    
    This is what likely happened in
    https://github.com/postfixadmin/postfixadmin/issues/30 (see the comment
    from Apr 24, 2018)

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Postfix Admin</title>
+</head>
+<body>
+    <p>The Postfix Admin directory layout changed.</p>
+    <p>Please update your webserver config so that the DocumentRoot or Alias points to the directory "public".</p>
+</body>
+</html>
