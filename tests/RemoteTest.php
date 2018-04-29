0d211949c965b21a213e4c9edd4823bbcab696fb
David Goodwin <david@palepurple.co.uk>
2018-04-29 20:16:02 +0100

    skip tests unless configuraton updated

--- tests/RemoteTest.php
+++ tests/RemoteTest.php
@@ -3,9 +3,9 @@
 require_once('common.php');
 
 abstract class RemoteTest extends PHPUnit_Framework_TestCase {
-    protected $server_url = 'http://orange/david/postfixadmin/xmlrpc.php';
-    protected $username = 'roger@example.com';
-    protected $password = 'patchthedog';
+    protected $server_url = 'http://change.me/to/work'; // http://orange/david/postfixadmin/xmlrpc.php';
+    protected $username = 'user@example.com';
+    protected $password = 'password1';
 
     /* xmlrpc objects... */
     protected $user;
@@ -14,6 +14,14 @@ abstract class RemoteTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         parent::setUp();
+
+        if($this->server_url == 'http://change.me/to/work') {
+            $this->markTestSkipped("Test skipped; Configuration change to \$this->server_url required");
+        }
+
+        if(!class_exists('Zend_XmlRpc_Client', true)) {
+            $this->markTestSkipped("Test skipped; Zend_XmlRpc_Client not found");
+        }
 
         $this->xmlrpc_client = new Zend_XmlRpc_Client($this->server_url);
         $http_client = $this->xmlrpc_client->getHttpClient();
