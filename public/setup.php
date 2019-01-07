cc079b773f948b880c71205ae2783a5eb86a99d0
David Goodwin <david@codepoets.co.uk>
2019-01-07 21:57:52 +0000

    fix formatting

--- public/setup.php
+++ public/setup.php
@@ -345,8 +345,7 @@ if ($error != 0) {
       <td colspan="3"><h4>Warning: connection not secure, switch to https if possible</h4></td>
    </tr>
 <?php
-   endif;
-?>
+   endif; ?>
    <tr>
       <td><label for="setup_password">Setup password</label></td>
       <td><input class="flat" type="password" name="setup_password" id="setup_password" value="" /></td>
@@ -386,8 +385,7 @@ if ($error != 0) {
       <td colspan="3"><h4>Warning: connection not secure, switch to https if possible</h4></td>
    </tr>
 <?php
-   endif;
-?>
+   endif; ?>
    <tr>
        <td><label for="setup_password">Setup password</label></td>
       <td><input id=setup_password class="flat" type="password" name="setup_password" value="" /></td>
