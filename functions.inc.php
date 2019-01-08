885bad949d3b75e4045c35446c3348ed4f55a116
Luca <volp1s@users.noreply.github.com>
2019-01-08 19:33:14 +0100

    Fix for MySQL 8
    The keyword ROW became reserved in MySQL 8.0.2
    https://dev.mysql.com/doc/refman/8.0/en/keywords.html#keywords-8-0-detailed-R

--- functions.inc.php
+++ functions.inc.php
@@ -525,7 +525,7 @@ function create_page_browser($idxfield, $querypart) {
     }
 
     # init row counter
-    $initcount = "SET @row=-1";
+    $initcount = "SET @r=-1";
     if (db_pgsql()) {
         $initcount = "CREATE TEMPORARY SEQUENCE rowcount MINVALUE 0";
     }
@@ -537,8 +537,8 @@ function create_page_browser($idxfield, $querypart) {
     $page_size_zerobase = $page_size - 1;
     $query = "
         SELECT * FROM (
-            SELECT $idxfield AS label, @row := @row + 1 AS 'row' $querypart
-        ) idx WHERE MOD(idx.row, $page_size) IN (0,$page_size_zerobase) OR idx.row = $count_results
+            SELECT $idxfield AS label, @r := @r + 1 AS 'r' $querypart
+        ) idx WHERE MOD(idx.r, $page_size) IN (0,$page_size_zerobase) OR idx.r = $count_results
     ";
 
     if (db_pgsql()) {
