d6fc3372729b501b62cea27ce8ff4ee879169064
David Goodwin <david@codepoets.co.uk>
2019-02-16 19:17:27 +0000

    dump mysql config things; lets fumble about in the dark once again and see...

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -55,7 +55,10 @@ if (getenv('DATABASE') == 'mysql') {
 
     $config = parse_ini_file($expand_tilde('~/.my.cnf'));
 
+    var_dump($config);
+
     if (empty($config)) {
+        var_dump("using default mysql config (localhost, root, empty password");
         $config = ['user'=>'root', 'host' => '127.0.0.1', 'password' => ''];
     }
 
