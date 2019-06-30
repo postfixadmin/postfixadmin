postfixadmin/postfixadmin

commit d809e0fbf7ccb80cf166b50761d0108dc10a0f5e

Adds colored indicators for password expired, account disabled and vacation enabled accounts

functions.inc.php
@@ -2201,6 +2201,39 @@ function gen_show_status($show_alias) {
         }
     }
 
+    // Vacation CHECK
+    if ( $CONF['show_vacation'] == 'YES' ) {
+                $stat_result = db_query ("SELECT * FROM ". $CONF['database_tables']['vacation'] ." WHERE email = '" . $show_alias . "' AND active = 1");
+            if ($stat_result['rows'] == 1) {
+            $stat_string .= "<span style='background-color:" . $CONF['show_vacation_color'] .
+                "'>" . $CONF['show_status_text'] . "</span>&nbsp;";
+        } else {
+            $stat_string .= $CONF['show_status_text'] . "&nbsp;";
+        }
+    }
+
+    // Disabled CHECK
+    if ( $CONF['show_disabled'] == 'YES' ) {
+                $stat_result = db_query ("SELECT * FROM ". $CONF['database_tables']['mailbox'] ." WHERE username = '" . $show_alias . "' AND active = 0");
+            if ($stat_result['rows'] == 1) {
+            $stat_string .= "<span style='background-color:" . $CONF['show_disabled_color'] .
+                "'>" . $CONF['show_status_text'] . "</span>&nbsp;";
+        } else {
+            $stat_string .= $CONF['show_status_text'] . "&nbsp;";
+        }
+    }
+
+    // Expired CHECK
+    if ( $CONF['show_expired'] == 'YES' ) {
+                $stat_result = db_query ("SELECT * FROM ". $CONF['database_tables']['mailbox'] ." WHERE username = '" . $show_alias . "' AND pw_expires_on <= now()");
+            if ($stat_result['rows'] == 1) {
+            $stat_string .= "<span style='background-color:" . $CONF['show_expired_color'] .
+                "'>" . $CONF['show_status_text'] . "</span>&nbsp;";
+        } else {
+            $stat_string .= $CONF['show_status_text'] . "&nbsp;";
+        }
+    }
+
     // POP/IMAP CHECK
     if ($CONF['show_popimap'] == 'YES') {
         $stat_delimiter = "";

config.inc.php
@@ -516,6 +516,16 @@ $CONF['show_undeliverable']='YES';
 $CONF['show_undeliverable_color']='tomato';
 // mails to these domains will never be flagged as undeliverable
 $CONF['show_undeliverable_exceptions']=array("unixmail.domain.ext","exchangeserver.domain.ext");
+// show mailboxes with expired password
+$CONF['show_expired']='YES';
+$CONF['show_expired_color']='orange';
+// show vacation enabled mailboxes
+$CONF['show_vacation']='YES';
+$CONF['show_vacation_color']='turquoise';
+// show disabled accounts
+$CONF['show_disabled']='YES';
+$CONF['show_disabled_color']='grey';
+// show POP/IMAP mailboxes
 $CONF['show_popimap']='YES';
 $CONF['show_popimap_color']='darkgrey';
 // you can assign special colors to some domains. To do this,

languages/en.lang
@@ -348,6 +348,10 @@ $PALANG['broadcast_mailboxes_only'] = 'Only send to mailboxes';
 $PALANG['broadcast_to_domains'] = 'Send to domains:';
 
 $PALANG['pStatus_undeliverable'] = 'maybe UNDELIVERABLE ';
+$PALANG['pStatus_disabled'] = 'Account disabled ';
+$PALANG['pStatus_expired'] = 'Password expired ';
+$PALANG['pStatus_vacation'] = 'Vacation enabled ';
+
 $PALANG['pStatus_custom'] = 'Delivers to ';
 $PALANG['pStatus_popimap'] = 'POP/IMAP ';
 

languages/fr.lang
@@ -342,6 +342,9 @@ $PALANG['pBroadcast_error_empty'] = 'Les champs "Nom", "Sujet" et "Message" ne p
 $PALANG['broadcast_mailboxes_only'] = 'Only send to mailboxes'; # XXX
 $PALANG['broadcast_to_domains'] = 'Send to domains:'; # XXX
 $PALANG['pStatus_undeliverable'] = 'Non délivrable ';
+$PALANG['pStatus_vacation'] = 'Répondeur activé ';
+$PALANG['pStatus_disabled'] = 'Compte désactivé ';
+$PALANG['pStatus_expired'] = 'Mot de passe expiré ';
 $PALANG['pStatus_custom'] = 'Délivré à ';
 $PALANG['pStatus_popimap'] = 'POP/IMAP ';
 $PALANG['password_too_short'] = 'Mot de passe trop court. - %s caractères minimum';

templates/list-virtual.tpl
@@ -71,6 +71,16 @@
 	{if $CONF.show_undeliverable===YES}
 		&nbsp;<span style='background-color:{$CONF.show_undeliverable_color};'>{$CONF.show_status_text}</span>={$PALANG.pStatus_undeliverable}
 	{/if}
+	{if $CONF.show_vacation===YES}
+                &nbsp;<span style='background-color:{$CONF.show_vacation_color};'>{$CONF.show_status_text}</span>={$PALANG.pStatus_vacation}
+        {/if}
+        {if $CONF.show_disabled===YES}
+                &nbsp;<span style='background-color:{$CONF.show_disabled_color};'>{$CONF.show_status_text}</span>={$PALANG.pStatus_disabled}
+        {/if}
+        {if $CONF.show_expired===YES}
+                &nbsp;<span style='background-color:{$CONF.show_expired_color};'>{$CONF.show_status_text}</span>={$PALANG.pStatus_expired}
+        {/if}
+
 	{if $CONF.show_popimap===YES}
 		&nbsp;<span style='background-color:{$CONF.show_popimap_color};'>{$CONF.show_status_text}</span>={$PALANG.pStatus_popimap}
 	{/if}

commit 55858bf3a0d33c7c9f9a9a58b43137579c4fdda3

Fix table names in gen_show_status()
... by using table_by_key() instead of reading $CONF directly.
Otherwise, $CONF['database_prefix'] gets ignored and the query tries to
use a wrong table name.

This fixes a bug introduced in d809e0fbf7ccb80cf166b50761d0108dc10a0f5e

functions.inc.php
@@ -2088,7 +2088,7 @@ function gen_show_status($show_alias) {
 
     // Vacation CHECK
     if ( $CONF['show_vacation'] == 'YES' ) {
-        $stat_result = db_query_one("SELECT * FROM ". $CONF['database_tables']['vacation'] ." WHERE email = ? AND active = ? ", array($show_alias, db_get_boolean(true) )) ;
+        $stat_result = db_query_one("SELECT * FROM ". table_by_key('vacation') ." WHERE email = ? AND active = ? ", array($show_alias, db_get_boolean(true) )) ;
         if (!empty($stat_result)) {
             $stat_string .= "<span style='background-color:" . $CONF['show_vacation_color'] . "'>" . $CONF['show_status_text'] . "</span>&nbsp;";
         } else {
@@ -2099,7 +2099,7 @@ function gen_show_status($show_alias) {
     // Disabled CHECK
     if ( $CONF['show_disabled'] == 'YES' ) {
         $stat_result = db_query_one(
-            "SELECT * FROM ". $CONF['database_tables']['mailbox'] ." WHERE username = ? AND active = ?",
+            "SELECT * FROM ". table_by_key('mailbox') ." WHERE username = ? AND active = ?",
             array($show_alias, db_get_boolean(false))
         );
         if (!empty($stat_result)) {
@@ -2116,7 +2116,7 @@ function gen_show_status($show_alias) {
             $now = "datetime('now')";
         }
 
-        $stat_result = db_query_one("SELECT * FROM ". $CONF['database_tables']['mailbox'] ." WHERE username = ? AND password_expiry <= ? AND active = ?", array( $show_alias , $now , db_get_boolean(true) ));
+        $stat_result = db_query_one("SELECT * FROM ". table_by_key('mailbox') ." WHERE username = ? AND password_expiry <= ? AND active = ?", array( $show_alias , $now , db_get_boolean(true) ));
 
         if (!empty($stat_result)) {
             $stat_string .= "<span style='background-color:" . $CONF['show_expired_color'] . "'>" . $CONF['show_status_text'] . "</span>&nbsp;";