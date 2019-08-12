c3d3a9dbc89bf290e56ccc0ff6f3dfc40a14280c
David Goodwin <david@codepoets.co.uk>
2019-08-12 10:24:42 +0100

    apply patch for vacation body encoding - see #288

--- VIRTUAL_VACATION/vacation.pl
+++ VIRTUAL_VACATION/vacation.pl
@@ -18,7 +18,7 @@
 
 use utf8;
 use DBI;
-use Encode qw(decode);
+use Encode qw(decode encode);
 use MIME::EncWords qw(:all);
 use Email::Valid;
 use strict;
@@ -30,7 +30,6 @@ use Email::Simple::Creator;
 use Try::Tiny;
 use Log::Log4perl qw(get_logger :levels);
 use File::Basename;
-
 # ========== begin configuration ==========
 
 # IMPORTANT: If you put passwords into this script, then remember
@@ -484,7 +483,7 @@ sub send_vacation_email {
                 'Content-Type' => "text/plain; charset=utf-8",
                 'X-Loop' => 'Postfix Admin Virtual Vacation',
             ],
-            body => $body,
+            body => encode('UTF-8', $body),
         );
 
         if($test_mode == 1) {
