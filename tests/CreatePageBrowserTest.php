a603d36224ee9ee01397b419e26eb8fd95bca89e
David Goodwin <david@codepoets.co.uk>
2019-09-17 20:46:52 +0100

    format

--- tests/CreatePageBrowserTest.php
+++ tests/CreatePageBrowserTest.php
@@ -27,7 +27,7 @@ class CreatePageBrowserTest extends \PHPUnit\Framework\TestCase {
         // this breaks on sqlite atm.
         $b = create_page_browser('mailbox.username', 'FROM mailbox WHERE 1 = 1');
         $this->assertEquals(10, sizeof($b));
-        foreach($b as $range) {
+        foreach ($b as $range) {
             $this->assertRegExp('/[\w]{2}\-[\w]{2}/', $range);
         }
         $this->assertNotEmpty($b);
