5e5e4a1c9b9d0e9ef591e52762c253f9be694cfe
David Goodwin <david@codepoets.co.uk>
2019-09-26 21:13:04 +0100

    fix var quoting

--- ADDITIONS/virtualmaildel.php
+++ ADDITIONS/virtualmaildel.php
@@ -50,13 +50,13 @@ function deldir($dir) {
     $current_dir = opendir($dir);
     while ($entryname = readdir($current_dir)) {
         if (is_dir("$dir/$entryname") and ($entryname != "." and $entryname!="..")) {
-            deldir("${dir}/${entryname}");
+            deldir("{$dir}/{$entryname}");
         } elseif ($entryname != "." and $entryname!="..") {
-            unlink("${dir}/${entryname}");
+            unlink("{$dir}/{$entryname}");
         }
     }
     closedir($current_dir);
-    @rmdir(${dir});
+    @rmdir($dir);
 }
 
 // --- Main Start ---
