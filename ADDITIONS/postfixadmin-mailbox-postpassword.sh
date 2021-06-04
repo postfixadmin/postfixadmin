768f2ee84449543d32d8b235450587c3d118cf17
BotoX <github@botox.bz>
2021-06-04 15:48:41 +0200

    Improve dovecot mail-crypt postpassword script security (pipe password instead of passing on cmdline) Suggested from: https://github.com/postfixadmin/postfixadmin/issues/441#issuecomment-774736944

--- ADDITIONS/postfixadmin-mailbox-postpassword.sh
+++ ADDITIONS/postfixadmin-mailbox-postpassword.sh
@@ -3,11 +3,14 @@
 # Example script for dovecot mail-crypt-plugin
 # https://doc.dovecot.org/configuration_manual/mail_crypt_plugin/
 
+IFS= read -r -d $'\0' OLD_PASSWORD
+IFS= read -r -d $'\0' NEW_PASSWORD
+
 # New user
-if [ -z "$3" ]; then
-    doveadm -o plugin/mail_crypt_private_password="$4" mailbox cryptokey generate -u "$1" -U
-    exit 0
+if [ -z "$OLD_PASSWORD" ]; then
+    OLD_PASSWORD="$(openssl rand -hex 16)"
+    doveadm -o plugin/mail_crypt_private_password="$OLD_PASSWORD" mailbox cryptokey generate -u "$1" -U
 fi
 
 # Password change
-doveadm mailbox cryptokey password -u "$1" -o "$3" -n "$4"
+printf "%s\n%s\n" "$OLD_PASSWORD" "$NEW_PASSWORD" | doveadm mailbox cryptokey password -u "$1" -N -O ""
