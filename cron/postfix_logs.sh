cbc1c78d58b034aa6c1109c99d8523bb90373e25
benchea dan <30780133+bnchdan@users.noreply.github.com>
2022-03-07 15:13:57 +0200

    Create postfix_logs.sh

---
+++ cron/postfix_logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#check if maillog directory exists
+if [ ! -d ../maillog ]
+then
+        #create direcotry
+	mkdir ../maillog
+fi
+
+
+data=`date --date='1 day ago' +%F`
+
+mysql -u root --skip-column-names --execute="USE postfix; SELECT username,domain FROM domain_admins WHERE active=1 AND domain <> 'ALL';" | while read username domain
+	    do
+
+		#check if folder for domain doesn t exists
+		if [ ! -d ../maillog/$domain ]
+		then
+    			echo "Directory does not exists for "$domain
+			#create direcotry
+			mkdir ../maillog/$domain			
+		fi
+
+		grep @$domain /var/log/mail.log.1 | grep -v "postfix-policyd" | grep -v postgrey > ../maillog/$domain/$data-bulk.log
+		pflogsumm -i --problems_first --detail 5 ../maillog/$domain/$data-bulk.log > ../maillog/$domain/$data-pflogsum.log 
+		grep @$domain /var/log/mail.log.1 | grep "Password mismatch" > ../maillog/$domain/$data-failed-auth.log
+
+		gzip ../maillog/$domain/$data-bulk.log
+		gzip ../maillog/$domain/$data-pflogsum.log
+		gzip ../maillog/$domain/$data-failed-auth.log
+		echo $domain
+		
+#		rm ../maillog/$domain/$data-bulk.log 
+#		rm ../maillog/$domain/$data-pflogsum.log 
+#		rm ../maillog/$domain/$data-failed-auth.log
+	    done
