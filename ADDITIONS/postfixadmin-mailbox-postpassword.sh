ce05c3e84407405c761481f923527adc02727b5a
David Goodwin <david@codepoets.co.uk>
2022-08-02 09:15:37 +0100

    see https://github.com/postfixadmin/postfixadmin/issues/646 - add note/comment, doing version parsing in bash to make the script handle it seems clumsy and verbose

--- ADDITIONS/postfixadmin-mailbox-postpassword.sh
+++ ADDITIONS/postfixadmin-mailbox-postpassword.sh
@@ -12,5 +12,8 @@ if [ -z "$OLD_PASSWORD" ]; then
     doveadm -o plugin/mail_crypt_private_password="$OLD_PASSWORD" mailbox cryptokey generate -u "$1" -U
 fi
 
+# If you're using dovecot >= 2.3.19, try this instead (See: https://github.com/postfixadmin/postfixadmin/issues/646)
+# printf "%s\n%s\n" "$OLD_PASSWORD" "$NEW_PASSWORD" "$NEW_PASSWORD" | doveadm mailbox cryptokey password -u "$1" -N -O 
+
 # Password change
 printf "%s\n%s\n" "$OLD_PASSWORD" "$NEW_PASSWORD" | doveadm mailbox cryptokey password -u "$1" -N -O ""
