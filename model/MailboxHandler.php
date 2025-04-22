30a24187fc1af9102eb4b4130ac68ffee3637255
David Goodwin <david@codepoets.co.uk>
2025-04-22 22:06:08 +0100

    It just wouldn't be right if I had remembered to format the code ...

--- model/MailboxHandler.php
+++ model/MailboxHandler.php
@@ -332,7 +332,7 @@ class MailboxHandler extends PFAHandler
                 $this->infomsg[] = Config::lang_f('pCreate_mailbox_result_succes_nosubfolders', $this->id);
             }
 
-            if(Config::has('mailbox_postcreation_hook')) {
+            if (Config::has('mailbox_postcreation_hook')) {
                 $mailbox_postcreation_hook = Config::read('mailbox_postcreation_hook');
                 if (!empty($mailbox_postcreation_hook) && is_callable($mailbox_postcreation_hook)) {
                     $mailbox_postcreation_hook($this->id, $this->values);
