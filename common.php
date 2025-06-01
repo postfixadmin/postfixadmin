a9215a99faf4ff3eb5da008609a3f9ae15cb18fa
David Goodwin <david@codepoets.co.uk>
2025-06-01 15:36:50 +0100

    change project reference

--- common.php
+++ common.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . '/vendor/autoload.php');
  * This source file is subject to the GPL license that is bundled with
  * this package in the file LICENSE.TXT.
  *
- * Further details on the project are available at http://postfixadmin.sf.net
+ * Further details on the project are available at https://github.com/postfixadmin/postfixadmin
  *
  * @license GNU GPL v2 or later.
  *
