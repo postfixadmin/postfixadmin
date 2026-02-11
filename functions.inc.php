af9974ba6555446c47cc07972a53040f5c2067e4
David Goodwin <david@codepoets.co.uk>
2026-02-11 22:06:22 +0000

    formatting

--- functions.inc.php
+++ functions.inc.php
@@ -777,7 +777,7 @@ function encode_header($string, $default_charset = "utf-8")
                     }
                 }
                 break;
-            # end switch
+                # end switch
         }
     }
     if ($enc_init) {
