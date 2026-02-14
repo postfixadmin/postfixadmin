0bbf8a32fc350cdc3d50dce5729e65c9baf54571
David Goodwin <david@codepoets.co.uk>
2026-02-14 16:48:34 +0000

    add unit test for CsrfToken class

--- tests/CsrfTokenTest.php
+++ tests/CsrfTokenTest.php
@@ -6,4 +6,45 @@ use PHPUnit\Framework\TestCase;
 class CsrfTokenTest extends TestCase
 {
 
+    public function testBasic()
+    {
+        $token1 = CsrfToken::generate();
+        $token2 = CsrfToken::generate();
+        $this->assertNotEquals($token1, $token2);
+        $this->assertNotEmpty($token1);
+        $this->assertNotEmpty($token2);
+
+        $this->assertTrue(CsrfToken::assertValid($token1, true));
+        $this->assertTrue(CsrfToken::assertValid($token2, true));
+
+        $this->expectException(CsrfInvalidException::class);
+        CsrfToken::assertValid("different token", true);
+
+    }
+
+    public function testThatTokenCanOnlyBeUsedOnce()
+    {
+        $token1 = CsrfToken::generate();
+
+        $this->assertNotEmpty($token1);
+        $this->assertTrue(CsrfToken::assertValid($token1));
+
+        $this->expectException(CsrfInvalidException::class);
+        $this->assertTrue(CsrfToken::assertValid($token1));
+
+    }
+
+
+    public function testThatOrderOfUseDoesNotMatter()
+    {
+        $token1 = CsrfToken::generate();
+        $token2 = CsrfToken::generate();
+
+        $this->assertNotEmpty($token1);
+        $this->assertNotEmpty($token2);
+
+        $this->assertTrue(CsrfToken::assertValid($token2));
+        $this->assertTrue(CsrfToken::assertValid($token1));
+
+    }
 }
