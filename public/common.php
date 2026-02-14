0fe968aaabb858dae7aeb99b79851ce2439c26d5
David Goodwin <david@codepoets.co.uk>
2026-02-14 16:49:15 +0000

    add a generic exception handler in public/common.php, so we can try and output something friendly if something goes wrong

--- public/common.php
+++ public/common.php
@@ -1,3 +1,18 @@
 <?php
 
 require_once(dirname(__FILE__) . '/../common.php');
+
+set_exception_handler(function ($exception) {
+
+    // we could do something much better here - e.g. if the user is logged in, and an admin, show a full stakc trace or something.
+    if ($exception instanceof CsrfInvalidException) {
+        http_response_code(419); // HTTP 419 Page Expired.
+        echo "<p><strong>PostfixAdmin Error:</strong></p><p>Invalid CSRF token. Try going back, and refreshing the page before trying again.</p>\n";
+        exit(0);
+    }
+
+    error_log("PostfixAdmin : untrapped exception. " . $exception->getMessage() . " - " . $exception->getTraceAsString());
+    http_response_code(500);
+    echo "<p><strong>PostfixAdmin Error:</strong></p><p>PostfixAdmin encountered an error. More details are in the server log files (e.g. /var/log/apache2/error.log).</p>";
+});
+
