d445dac9168d6f54c22a2399de488c8a1b825754
David Goodwin <david@codepoets.co.uk>
2026-02-28 13:54:41 +0000

    fix sql in clause so global search works again

--- public/main.php
+++ public/main.php
@@ -43,18 +43,15 @@ $smarty->assign('q', '');
 // do not run this search stuff for an admin who has no domains associated.
 if (!empty($q) && !empty($list_domains)) {
 
-    $domains_sql = db_in_clause('domain', $list_domains);
-
-
-    $domain_filter = " AND domain IN ( $domains_sql ) ";
+    $domain_filter = db_in_clause('domain', $list_domains);
 
     $table_alias = table_by_key('alias');
     $table_domain = table_by_key('domain');
     $table_mailbox = table_by_key('mailbox');
 
-    $mailboxes = db_query_all("SELECT * FROM $table_mailbox WHERE username LIKE :q $domain_filter ORDER BY username ASC LIMIT 15", ['q' => "%$q%"]);
-    $aliases = db_query_all("SELECT * FROM $table_alias WHERE address LIKE :q $domain_filter ORDER BY address ASC LIMIT 15", ['q' => "%$q%"]);
-    $domains = db_query_all("SELECT * FROM $table_domain WHERE domain LIKE :q $domain_filter AND domain != 'ALL'  ORDER BY domain ASC LIMIT 15", ['q' => "%$q%"]);
+    $aliases = db_query_all("SELECT * FROM $table_alias WHERE address LIKE :q AND $domain_filter ORDER BY address ASC LIMIT 15", ['q' => "%$q%"]);
+    $mailboxes = db_query_all("SELECT * FROM $table_mailbox WHERE username LIKE :q AND $domain_filter ORDER BY username ASC LIMIT 15", ['q' => "%$q%"]);
+    $domains = db_query_all("SELECT * FROM $table_domain WHERE domain LIKE :q AND $domain_filter AND domain != 'ALL'  ORDER BY domain ASC LIMIT 15", ['q' => "%$q%"]);
 
     $smarty->assign('q', $q);
     $smarty->assign('mailboxes', $mailboxes);
